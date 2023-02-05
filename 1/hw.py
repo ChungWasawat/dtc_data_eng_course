d30e5b46a294e5980712015be0f2d83f84ecea1f
wasawat <bun.wasawat@outlook.com>
2023-02-05 22:45:29 +0000

    hw2

--- 1/hw.py
+++ 1/hw.py
@@ -1,3 +1,5 @@
+from hashlib import sha1
 
-wget https://github.com/DataTalksClub/nyc-tlc-data/releases/download/green/green_tripdata_2019-01.csv.gz
-wget https://s3.amazonaws.com/nyc-tlc/misc/taxi+_zone_lookup.csv
+def hashit(data):
+    return sha1(data.lower().encode('utf-8')).hexdigest()
+    
