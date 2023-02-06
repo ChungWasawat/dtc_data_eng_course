ff9754afe3c92c962d8a7da4298d2b283851e657
wasawat <bun.wasawat@outlook.com>
2023-02-06 14:08:46 +0000

    web to gcs

---
+++ postgresql/etl_web_to_gcs.py
@@ -0,0 +1,69 @@
+from pathlib import Path
+import pandas as pd
+from prefect import flow, task
+from prefect_gcp.cloud_storage import GcsBucket
+from random import randint
+
+
+@task(retries=3)
+def fetch(dataset_url: str) -> pd.DataFrame:
+    """Read taxi data from web into pandas DataFrame"""
+    # if randint(0, 1) > 0:
+    #     raise Exception
+
+    df = pd.read_csv(dataset_url)
+    return df
+
+
+@task(log_prints=True)
+def clean(df: pd.DataFrame, colour: str) -> pd.DataFrame:
+    """Fix dtype issues"""
+    if colour == "yellow":
+        df["tpep_pickup_datetime"] = pd.to_datetime(df["tpep_pickup_datetime"])
+        df["tpep_dropoff_datetime"] = pd.to_datetime(df["tpep_dropoff_datetime"])
+    elif colour == "green":
+        df["lpep_pickup_datetime"] = pd.to_datetime(df["lpep_pickup_datetime"])
+        df["lpep_dropoff_datetime"] = pd.to_datetime(df["lpep_dropoff_datetime"])
+    print(df.head(2))
+    print(f"columns: {df.dtypes}")
+    print(f"rows: {len(df)}")
+    return df
+
+
+@task()
+def write_local(df: pd.DataFrame, color: str, dataset_file: str) -> Path:
+    """Write DataFrame out locally as parquet file"""
+    local_path = Path.cwd()
+    path = Path(f"data/{color}/{dataset_file}.parquet")
+    path2 = local_path / path
+
+    df.to_parquet(path2, compression="gzip")
+    return path2
+
+
+@task()
+def write_gcs(path: Path, colour: str, dataset_file: str) -> None:
+    """Upload local parquet file to GCS"""
+    gcs_block = GcsBucket.load("dtcde-prefect-gcs")
+    gBucket_path = f"data/{colour}/{dataset_file}.parquet"
+    gcs_block.upload_from_path(from_path=path, to_path=gBucket_path)
+    return
+
+
+@flow()
+def etl_web_to_gcs() -> None:
+    """The main ETL function"""
+    color = "yellow"
+    year = 2021
+    month = 1
+    dataset_file = f"{color}_tripdata_{year}-{month:02}"
+    dataset_url = f"https://github.com/DataTalksClub/nyc-tlc-data/releases/download/{color}/{dataset_file}.csv.gz"
+
+    df = fetch(dataset_url)
+    df_clean = clean(df, color)
+    path = write_local(df_clean, color, dataset_file)
+    write_gcs(path, color, dataset_file)
+
+
+if __name__ == "__main__":
+    etl_web_to_gcs()
---
+++ postgresql/test.py
@@ -0,0 +1,7 @@
+# import os
+# from pathlib import Path
+
+# print(os.getcwd())
+# print( Path.cwd())
+
+# print(Path(f"data/lop/loop.parquet"))
