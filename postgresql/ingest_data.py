cc83b1dee4040e06227b03cd1d4c933571085ad9
wasawat <bun.wasawat@outlook.com>
2023-02-06 02:19:01 +0000

    hw2 q1&2

--- postgresql/ingest_data.py
+++ postgresql/ingest_data.py
@@ -12,7 +12,8 @@ def ingest_data(user, password, host, port, db, table_name, url):
     # the backup files are gzipped, and it's important to keep the correct extension
     # for pandas to be able to open the file
     if url.endswith('.csv.gz'):
-        csv_name = 'yellow_tripdata_2020-01.csv.gz'
+        # csv_name = 'yellow_tripdata_2020-01.csv.gz'
+        csv_name = 'green_tripdata_2020-01.csv.gz'
     else:
         csv_name = 'output.csv'
 
@@ -24,13 +25,17 @@ def ingest_data(user, password, host, port, db, table_name, url):
 
     df = next(df_iter)
 
-    df.tpep_pickup_datetime = pd.to_datetime(df.tpep_pickup_datetime)
-    df.tpep_dropoff_datetime = pd.to_datetime(df.tpep_dropoff_datetime)
+    #yellow taxi
+    #df.tpep_pickup_datetime = pd.to_datetime(df.tpep_pickup_datetime)
+    #df.tpep_dropoff_datetime = pd.to_datetime(df.tpep_dropoff_datetime)
+
+    #green taxi
+    df.lpep_pickup_datetime = pd.to_datetime(df.lpep_pickup_datetime)
+    df.lpep_dropoff_datetime = pd.to_datetime(df.lpep_dropoff_datetime)
 
     df.head(n=0).to_sql(name=table_name, con=engine, if_exists='replace')
 
     df.to_sql(name=table_name, con=engine, if_exists='append')
-
 
     while True: 
 
@@ -39,8 +44,13 @@ def ingest_data(user, password, host, port, db, table_name, url):
             
             df = next(df_iter)
 
-            df.tpep_pickup_datetime = pd.to_datetime(df.tpep_pickup_datetime)
-            df.tpep_dropoff_datetime = pd.to_datetime(df.tpep_dropoff_datetime)
+            #yellow taxi
+            #df.tpep_pickup_datetime = pd.to_datetime(df.tpep_pickup_datetime)
+            #df.tpep_dropoff_datetime = pd.to_datetime(df.tpep_dropoff_datetime)
+
+            #green taxi
+            df.lpep_pickup_datetime = pd.to_datetime(df.lpep_pickup_datetime)
+            df.lpep_dropoff_datetime = pd.to_datetime(df.lpep_dropoff_datetime)
 
             df.to_sql(name=table_name, con=engine, if_exists='append')
 
@@ -53,12 +63,13 @@ def ingest_data(user, password, host, port, db, table_name, url):
             break
 
 if __name__ == '__main__':
-    user = "postgres"
-    password = "admin"
+    user = "root"
+    password = "root"
     host = "localhost"
-    port = "5433"
+    port = "5432"
     db = "ny_taxi"
-    table_name = "yellow_taxi_trips"
-    csv_url = "https://github.com/DataTalksClub/nyc-tlc-data/releases/download/yellow/yellow_tripdata_2020-01.csv.gz"
+    # table_name = "yellow_taxi_trips"
+    table_name = "green_trips"
+    csv_url = "https://github.com/DataTalksClub/nyc-tlc-data/releases/download/green/green_tripdata_2020-01.csv.gz"
 
     ingest_data(user, password, host, port, db, table_name, csv_url)
