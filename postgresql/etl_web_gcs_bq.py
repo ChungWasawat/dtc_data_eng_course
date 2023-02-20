555798e808843786b4af8b80052485d1800b91f4
wasawat <bun.wasawat@outlook.com>
2023-02-20 21:46:16 +0000

    upload data to gcs

--- postgresql/etl_web_gcs_bq.py
+++ postgresql/etl_web_gcs_bq.py
@@ -29,6 +29,24 @@ def clean(df: pd.DataFrame, colour: str) -> pd.DataFrame:
     elif colour == "green":
         df["lpep_pickup_datetime"] = pd.to_datetime(df["lpep_pickup_datetime"])
         df["lpep_dropoff_datetime"] = pd.to_datetime(df["lpep_dropoff_datetime"])
+        df["trip_type"] = df["trip_type"].astype('Int64')
+    elif colour == "fhv":
+        """Rename columns"""
+        df.rename({'dropoff_datetime':'dropOff_datetime'}, axis='columns', inplace=True)
+        df.rename({'PULocationID':'PUlocationID'}, axis='columns', inplace=True)
+        df.rename({'DOLocationID':'DOlocationID'}, axis='columns', inplace=True)
+
+        df["pickup_datetime"] = pd.to_datetime(df["pickup_datetime"])
+        df["dropOff_datetime"] = pd.to_datetime(df["dropOff_datetime"])
+    
+    if color == "yellow" or color == "green":
+        df["VendorID"] = df["VendorID"].astype('Int64')
+        df["RatecodeID"] = df["RatecodeID"].astype('Int64')
+        df["PULocationID"] = df["PULocationID"].astype('Int64')
+        df["DOLocationID"] = df["DOLocationID"].astype('Int64')
+        df["passenger_count"] = df["passenger_count"].astype('Int64')
+        df["payment_type"] = df["payment_type"].astype('Int64')
+        
     print(df.head(2))
     print(f"columns: {df.dtypes}")
     print(f"rows: {len(df)}")
@@ -65,12 +83,13 @@ def extract_from_gcs(color: str, year: int, month: int) -> Path:
 
 
 @task()
-def transform(path: Path) -> pd.DataFrame:
+def transform(path: Path, colour: str) -> pd.DataFrame:
     """Data cleaning example"""
-    df = pd.read_parquet(path)
-    print(f"pre: missing passenger count: {df['passenger_count'].isna().sum()}")
-    df["passenger_count"].fillna(0, inplace=True)
-    print(f"post: missing passenger count: {df['passenger_count'].isna().sum()}")
+    if colour == "yellow" or 'green':
+        df = pd.read_parquet(path)
+        print(f"pre: missing passenger count: {df['passenger_count'].isna().sum()}")
+        df["passenger_count"].fillna(0, inplace=True)
+        print(f"post: missing passenger count: {df['passenger_count'].isna().sum()}")
     return df
 
 @task()
@@ -99,55 +118,61 @@ def write_bq_xtable(colour: str) -> None:
     """Write DataFrame to BiqQuery as external table"""
     with BigQueryWarehouse.load("bigquery") as warehouse:
         create_operation = '''
-        CREATE TABLE IF NOT EXISTS mydataset.mytable (
-            col1 STRING,
-            col2 INTEGER,
-            col3 BOOLEAN
-        )
+        CREATE OR REPLACE EXTERNAL TABLE `mp-dtc-data-eng.eu_dtcDE_zoomcamp.external_fhv`
+        OPTIONS (
+            format = 'CSV',
+            uris = ['gs://nyc-tl-data/trip data/fhv_tripdata_2019-*.csv']
+        );
         '''
         warehouse.execute(create_operation)
-        insert_operation = '''
-        INSERT INTO mydataset.mytable (col1, col2, col3) VALUES (%s, %s, %s)
-        '''
-        seq_of_parameters = [
-            ("a", 1, True),
-            ("b", 2, False),
-        ]
-        warehouse.execute_many(
-            insert_operation,
-            seq_of_parameters=seq_of_parameters
-        )
+
+        # insert_operation = '''
+        # INSERT INTO mydataset.mytable (col1, col2, col3) VALUES (%s, %s, %s)
+        # '''
+        # seq_of_parameters = [
+        #     ("a", 1, True),
+        #     ("b", 2, False),
+        # ]
+        # warehouse.execute_many(
+        #     insert_operation,
+        #     seq_of_parameters=seq_of_parameters
+        # )
 
 
 ############################################# flow
 @flow()
-def etl_web_to_bq(year: int, month: int, color: str) -> None:
+def etl_web_to_bq(year: int, month: int, color: str, func: int) -> None:
     """The main ETL function"""
     dataset_file = f"{color}_tripdata_{year}-{month:02}"
     dataset_url = f"https://github.com/DataTalksClub/nyc-tlc-data/releases/download/{color}/{dataset_file}.csv.gz"
 
     # web to gcs
-    df = fetch(dataset_url)
-    df_clean = clean(df, color)
-    path = write_local(df_clean, color, dataset_file)
-    write_gcs(path, color, dataset_file)
-
+    if func == 0:
+        df = fetch(dataset_url)
+        df_clean = clean(df, color)
+        path = write_local(df_clean, color, dataset_file)
+        write_gcs(path, color, dataset_file)
     # gcs to bq
-    #path2 = extract_from_gcs(color, year, month)
-    #df2 = transform(path2)
-    #write_bq_table(df2, color)
-    write_bq_xtable(color)
+    elif func == 1:
+        path = extract_from_gcs(color, year, month)
+        df = transform(path, color)
+        write_bq_table(df, color)
+    # bq x table
+    elif func == 2:
+        write_bq_xtable(color)
 
 @flow()
 def etl_parent_w2bq_flow(
-    months: list[int] = [1, 2], year: int = 2021, color: str = "yellow"
+    months: list[int] = [1, 2], year: int = 2021, color: str = "yellow", func: int = 0
 ):
     for month in months:
-        etl_web_to_bq(year, month, color)
-
+        etl_web_to_bq(year, month, color, func)
 
 if __name__ == "__main__":
-    color = "yellow"
-    months = [1, 2, 3]
-    year = 2021
-    etl_parent_w2bq_flow(months, year, color)
+    color = "fhv"
+    # months = [1, 2, 3]
+    months = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+    year = 2019
+    # func = 0(web to gcs) / 1(gcs to bq) / 2(bq x table)
+    func = 0
+    etl_parent_w2bq_flow(months, year, color, func)
